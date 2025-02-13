df6f70716f7f0f5ad53824495dcbfd97923d1b4d
maxsoulfly <max.kibinimat@gmail.com>
2025-02-13 17:36:42 +0200

    Created files added 4 basic operations

---
+++ script.js
@@ -0,0 +1,25 @@
+
+let number1;
+let number2;
+let operator;
+
+// Function to add two numbers
+const add = (num1, num2) => {
+    return num1 + num2;
+}
+
+// Function to subtract the second number from the first
+const subtract = (num1, num2) => {
+    return num1 - num2;
+}
+
+// Function to multiply two numbers
+const multiply = (num1, num2) => {
+    return num1 * num2;
+}
+
+// Function to divide the first number by the second
+const divide = (num1, num2) => {
+    return num1 / num2;
+}
+
