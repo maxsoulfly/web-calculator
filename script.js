5555af8f0b765822826578c2fe02a5d5897a6eae
maxsoulfly <max.kibinimat@gmail.com>
2025-02-13 18:46:29 +0200

    Built a structured html and some additional CSS for calculator design

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ const multiply = (num1, num2) => {
 
 // Function to divide the first number by the second
 const divide = (num1, num2) => {
+    if (num2 == 0) return "ERROR: Cannot divide by zero";
     return num1 / num2;
 }
 
